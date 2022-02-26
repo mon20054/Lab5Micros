cdba57b1dd417cc38fab013b0863f72cd7596e2e
josej <josej@LAPTOP-680DDUI3>
2022-02-26 09:53:04 -0600

    Postlaboratorio 5 Javier Monzón 20054

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Postlab5JavierMonzon20054.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Postlab5JavierMonzon20054.X.production.hex
